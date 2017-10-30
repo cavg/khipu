d6ee25e33c6fa598a4a824ff7075652638e0c458
Camilo Verdugo <camilo.verdugo@gmail.com>
2017-10-30 08:43:43 -0300

    update version fix python3 version

--- setup.py
+++ setup.py
@@ -17,11 +17,11 @@ setup(
     # long_description=open("README.md").read(),
     classifiers=[
         'Development Status :: 3 - Alpha'
-        'Programming Language :: Python :: 2.7'
+        # 'Programming Language :: Python :: 2.7'
         # 'License :: OSI Approved :: Apache Software License',
         # 'Programming Language :: Python :: 2.6',
         # 'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
         'requests',
